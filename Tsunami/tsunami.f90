3bbbda2c153fbd9364623e47e01620e6421e2f55
Ali Golestani <golestani_ali@mathdep.iust.ac.ir>
2023-03-22 19:18:02 +0330

    initialization

---
+++ Tsunami/tsunami.f90
@@ -0,0 +1,21 @@
+program tsunami
+
+    implicit none
+
+    integer :: i, n 
+    integer :: grid_size
+    integer :: num_time_steps
+
+    real :: dt
+    real :: dx
+    real :: c
+
+    grid_size = 100
+    num_time_steps = 100
+
+    dt = 1.
+    dx = 1.
+    c = 1.
+    
+
+end program tsunami
