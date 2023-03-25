32ddc00832a119944e61499de0ee57f615033742
golestani-ali <golestani_ali@mathdep.iust.ac.ir>
2023-03-26 00:01:03 +0330

    init wave heights

--- Tsunami/tsunami.f90
+++ Tsunami/tsunami.f90
@@ -5,11 +5,14 @@ program tsunami
     integer :: i,n 
     integer, parameter :: grid_size
     integer, parameter :: num_time_steps
+    integer, parameter :: icenter = 25
+    integer, parameter :: decay = 0.02
 
     real, parameter :: dt, dx, c
 
     grid_size = 100
     num_time_steps = 100
+
 
     dt = 1.
     dx = 1.
@@ -21,6 +24,13 @@ program tsunami
     if (dt <= 0) stop "dt must be greater than zero!"
     if (dx <= 0) stop "dx must be greater than zero!"
     if (c <= 0) stop "c must be greater than zero!"
+
+    init_wave: do i = 1, grid_size
+        h(i) = exp(-decay * (i - icenter)**2)
+    end do init_wave
+    
+
+
 
 
 
