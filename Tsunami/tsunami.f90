b1030b956e7d2d199414fdb02f6eefd88f73d6d1
golestani-ali <golestani_ali@mathdep.iust.ac.ir>
2023-03-25 21:09:04 +0330

    initializing variables

--- Tsunami/tsunami.f90
+++ Tsunami/tsunami.f90
@@ -2,13 +2,11 @@ program tsunami
 
     implicit none
 
-    integer :: i, n 
-    integer :: grid_size
-    integer :: num_time_steps
+    integer :: i,n 
+    integer, parameter :: grid_size
+    integer, parameter :: num_time_steps
 
-    real :: dt
-    real :: dx
-    real :: c
+    real, parameter :: dt, dx, c
 
     grid_size = 100
     num_time_steps = 100
